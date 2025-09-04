81866916df60e2acdd9feeeacb4a095f545bec20
nandavarampritham <nandavarampritham@gmail.com>
2025-09-04 18:06:06 +0530

    Fix Render deployment configuration to point to server directory

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script directs Render to the correct directory
+
+# Change to the server directory
+cd server
+
+# Install dependencies
+npm install
+
+# The start command will be handled by Render's configuration
